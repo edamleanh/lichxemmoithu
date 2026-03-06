71b2f33bb570ff96bd0d06b8a174436382360185
DucAnh <edamleanh@gmail.com>
2026-03-06 14:09:20 +0700

    fix(mobile): svg transform config added to metro for valorant logos

--- lich-tong-hop-mobile/metro.config.js
+++ lich-tong-hop-mobile/metro.config.js
@@ -15,4 +15,16 @@ config.resolver.nodeModulesPaths = [
   path.resolve(workspaceRoot, 'node_modules'),
 ];
 
+// SVG Transformer Config
+const { transformer, resolver } = config;
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+};
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"]
+};
+
 module.exports = config;
